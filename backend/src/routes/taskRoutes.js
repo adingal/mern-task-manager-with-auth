3178b112bf2f19b26b45a69a641ea9a3065dd205
Lhex <xelalagnid@gmail.com>
2026-05-02 02:22:56 +0800

    Create: Initial routes

---
+++ backend/src/routes/taskRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/taskController')
+
+const router = express.Router()
+
+router.route('/').get(getAllTasks).post(createTask)
+
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+module.exports = router
