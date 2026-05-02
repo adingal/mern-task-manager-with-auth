8170acf8422bce5570753d1dd39eddd45055aa85
Lhex <xelalagnid@gmail.com>
2026-05-02 13:10:49 +0800

    Update: Schema validators

--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
+  title: {
+    type: String,
+    unique: true,
+    required: [true, 'A task must have a name.'],
+    trim: true,
+    minLength: [10, 'A task name must have 10 or more characters.'],
+    maxLength: [40, 'A task name must have 40 or less characters.'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    minLength: [10, 'A task name must have 10 or more characters.'],
+    maxLength: [120, 'A task name must have 40 or less characters.'],
+  },
   completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   __v: {
