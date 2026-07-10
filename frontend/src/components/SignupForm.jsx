8acc7ccf9c19b40873e46b42413181392431aa81
Lhex <xelalagnid@gmail.com>
2026-07-10 23:52:54 +0800

    Update: Integrate validation and error message - signup form

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import { Link } from 'react-router'
 
 const SignupForm = () => {
@@ -9,6 +10,18 @@ const SignupForm = () => {
       password: '',
       passwordConfirm: '',
     },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address.')
+        .required('Email is required.'),
+      password: Yup.string()
+        .min(8, 'A password must have atleast 8 or more characters.')
+        .max(30, 'A password must have a max of 30 or less characters.')
+        .required('Password is required.'),
+      passwordConfirm: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match.')
+        .required('Confirm password is required.'),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     },
@@ -19,7 +32,10 @@ const SignupForm = () => {
       onSubmit={formik.handleSubmit}
       className="bg-gray-100 p-6 rounded-sm flex flex-col gap-3"
     >
-      <div className="flex flex-col gap-0">
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-500 text-center mb-4">
+        Signup
+      </h1>
+      <div className="relative flex flex-col gap-0 pb-4">
         <label
           htmlFor="email"
           className="text-xs md:text-sm lg:text-base text-gray-400 mb-0"
@@ -32,11 +48,17 @@ const SignupForm = () => {
           type="email"
           onChange={formik.handleChange}
           value={formik.values.email}
+          onBlur={formik.handleBlur}
           className="border border-gray-300 rounded-sm p-3 outline-gray-400"
         />
+        {formik.touched.email && formik.errors.email ? (
+          <p className="absolute bottom-0 text-xs text-red-500">
+            {formik.errors.email}
+          </p>
+        ) : null}
       </div>
 
-      <div className="flex flex-col gap-0">
+      <div className="relative flex flex-col gap-0 pb-4">
         <label
           htmlFor="password"
           className="text-xs md:text-sm lg:text-base text-gray-400 mb-0"
@@ -49,25 +71,37 @@ const SignupForm = () => {
           type="password"
           onChange={formik.handleChange}
           value={formik.values.password}
+          onBlur={formik.handleBlur}
           className="border border-gray-300 rounded-sm p-3 outline-gray-400"
         />
+        {formik.touched.password && formik.errors.password ? (
+          <p className="absolute bottom-0 text-xs text-red-500">
+            {formik.errors.password}
+          </p>
+        ) : null}
       </div>
 
-      <div className="flex flex-col gap-0">
+      <div className="relative flex flex-col gap-0 pb-4">
         <label
-          htmlFor="password-confirm"
+          htmlFor="passwordConfirm"
           className="text-xs md:text-sm lg:text-base text-gray-400 mb-0"
         >
           Password Confirm
         </label>
         <input
-          id="password-confirm"
-          name="password-confirm"
+          id="passwordConfirm"
+          name="passwordConfirm"
           type="password"
           onChange={formik.handleChange}
           value={formik.values.passwordConfirm}
+          onBlur={formik.handleBlur}
           className="border border-gray-300 rounded-sm p-3 outline-gray-400"
         />
+        {formik.touched.passwordConfirm && formik.errors.passwordConfirm ? (
+          <p className="absolute bottom-0 text-xs text-red-500">
+            {formik.errors.passwordConfirm}
+          </p>
+        ) : null}
       </div>
 
       <button
