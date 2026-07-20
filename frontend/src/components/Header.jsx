cfb9db3ce3bebe17a2686e9f9ae41b84c3f5312a
Lhex <xelalagnid@gmail.com>
2026-07-20 20:35:26 +0800

    Update: Add logout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { logout } from '../store/auth/authSlice'
 import Container from './Container'
 
 function Header() {
+  const dispatch = useDispatch()
+  const { isAuthenticated } = useSelector((state) => state.user)
+
   return (
     <header className="bg-gray-700 mb-4">
       <Container>
-        <p className="text-xl md:text-2xl lg:text-3xl text-center font-semibold font-serif tracking-wide text-gray-200">
-          Task Manager App
-        </p>
+        <div
+          className={`flex flex-row justify-${isAuthenticated ? 'between' : 'center'}`}
+        >
+          <p className="text-xl md:text-2xl lg:text-3xl text-center font-semibold font-serif tracking-wide text-gray-200">
+            Task Manager App
+          </p>
+          {isAuthenticated && (
+            <div>
+              <button
+                className="text-sm md:text-base text-white hover:text-gray-300 transition-colors cursor-pointer"
+                type="button"
+                onClick={() => dispatch(logout())}
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
       </Container>
     </header>
   )
